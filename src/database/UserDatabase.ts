63535cf327a55f279042c6a0963cf9cf851b2d8a
Afmjuniors <afmjuniors@gmail.com>
2023-02-13 17:06:35 -0300

    mod

--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -1,33 +1,46 @@
 import { UserDB } from "../types"
 import { BaseDatabase } from "./BaseDatabase"
 
-export class UserDatabase extends BaseDatabase{
-    public static TABLE_USER= "users"
+export class UserDatabase extends BaseDatabase {
+    public static TABLE_USER = "users"
 
 
-    public getAllUsers =async ():Promise<UserDB[]>=>{
-        const result:UserDB[] = await BaseDatabase
-        .connection(UserDatabase.TABLE_USER)
+    public getAllUsers = async (): Promise<UserDB[]> => {
+        const result: UserDB[] = await BaseDatabase
+            .connection(UserDatabase.TABLE_USER)
         return result
     }
-    public getUserById =async (id:string) :Promise<UserDB| undefined>=> {
+    public getUserById = async (id: string): Promise<UserDB | undefined> => {
         const [user] = await BaseDatabase
-        .connection(UserDatabase.TABLE_USER)
-        .where({id})
+            .connection(UserDatabase.TABLE_USER)
+            .where({ id })
         return user
     }
-    public getUserByEmail = async (email:string):Promise<UserDB| undefined>=>{
-        
-        const [result] : UserDB[] = await BaseDatabase
-        .connection(UserDatabase.TABLE_USER)
-        .where({email})
+    public getUserByEmail = async (email: string): Promise<UserDB | undefined> => {
+
+        const [result]: UserDB[] = await BaseDatabase
+            .connection(UserDatabase.TABLE_USER)
+            .where({ email })
         return result
     }
-    public insertUser =async (user:UserDB):Promise<void> => {
+    public insertUser = async (user: UserDB): Promise<void> => {
+        await BaseDatabase
+            .connection(UserDatabase.TABLE_USER)
+            .insert(user)
+
+    }
+    public editUser = async (id: string, user: UserDB): Promise<void> => {
+        await BaseDatabase
+            .connection(UserDatabase.TABLE_USER)
+            .update(user)
+            .where({ id })
+
+    }
+    public deleteUser =async (id:string) => {
         await BaseDatabase
         .connection(UserDatabase.TABLE_USER)
-        .insert(user)
-        
+        .del()
+        .where({id})
     }
 
 
